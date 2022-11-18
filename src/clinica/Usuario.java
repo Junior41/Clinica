a1b18f7bd53b3bbf36fe7492d4c2d7cb6843f1e2
Junior41 <juniorbrandao41@hotmail.com>
2022-11-18 10:45:57 -0300

    adicionar usuario

---
+++ src/clinica/Usuario.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinica;
+
+/**
+ *
+ * @author junior
+ */
+public class Usuario {
+    private String nome, senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
